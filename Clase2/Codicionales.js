f3d5938a0d8b5e6efd3f9874c02140112a0f79b7
Agustin Borio <aguborio18@gmail.com>
2022-04-19 11:26:27 -0300

    ejercicios

---
+++ Clase2/Codicionales.js
@@ -0,0 +1,7 @@
+let color = prompt("Ingrese un color:")
+
+if(color === "verde" || "celeste"){
+    alert("verdadero");
+}else{
+    alert("chupame el pingo gil no es rojo")
+}
---
+++ index.js
@@ -0,0 +1,16 @@
+// let apellido = "Borio";
+
+// console.log("1", apellido)
+
+// apellido = "vela";
+
+// console.log("2", apellido)
+
+// let nombre = "Agustin";
+// let apeliido = "borio";
+
+// console.log(nombre, apeliido)
+
+
+// let result = prompt("por favor ingrese su nombre:")
+// alert(result)
